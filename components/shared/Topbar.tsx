c3b6ad575594b937f0be83aabda5fe300b198639
anwar ahmed <muhammadanwar.symits@gmail.com>
2023-08-10 11:54:21 +0500

    leftsidebar done

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -12,6 +13,30 @@ const Topbar = () => {
           Threads{" "}
         </p>
       </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4 text-white",
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 };
